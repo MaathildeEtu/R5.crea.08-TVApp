a9f5de60b71f6fdafac1afac363fa42cc5416313
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-10-15 09:21:16 +0200

    Phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { Button } from "@/components/ui/button";
+import useFetch from "./hook/useFetch";
 
 export default function Home() {
+  const { data, isLoading, error } = useFetch ("/data.json");
+
   return (
     <div>
-      <Button>Click me</Button>
+      <div className=" relative">
+        <img className="" src={data?.image.original} alt="" />
+      </div>
+      
+      <p className="text-center text-xl">{data?.genres[0]} - {data?.genres[1]} - {data?.genres[2]}</p>
+      <p className="text-center text-xl">{data?.schedule.days} - {data?.schedule.time}</p>
+      <p className="text-center text-xl">{data?.language}</p>
+
+      <h2 className="text-center text-xl">{data?.name}</h2>
+      <p className="text-center text-xl">{data?.language}</p>
+      <p className="text-center text-xl">{data?.rating.average}</p>
+
+      <p className="text-center text-xl">{data?.summary}</p>
+
+      <Button>
+        <img src="/play.svg" alt="" />
+        Play</Button>
+        
+      <p className="text-center text-xl">{data?._links.previousepisode.name}</p>
     </div>
   );
 }
