5233f017464a1dd352ac85ad8432dec27a6d7c3f
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-10-15 09:59:42 +0200

    Actu

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,33 @@ export default function Home() {
   const { data, isLoading, error } = useFetch ("/data.json");
 
   return (
-    <div>
-      <div className=" relative">
-        <img className=" absolute" src={data?.image.original} alt="" />
-        <p className="text-center text-xl">{data?.genres[0]} - {data?.genres[1]} - {data?.genres[2]}</p>
-        <p className="text-center text-xl">{data?.schedule.days} - {data?.schedule.time}</p>
-        <p className="text-center text-xl">{data?.language}</p>
+    <div className=" p-5 ">
+      <div className=" relative overflow-hidden flex flex-col gap-2 pt-36 p-6 justify-center items-center rounded-lg">
+        <img className=" absolute -z-10" src={data?.image.original} alt="" />
+
+        <div className=" flex flex-col items-start justify-start w-full">
+          <p className=" text-md">{data?.genres[0]} · {data?.genres[1]} · {data?.genres[2]}</p>
+          <div className=" flex flex-row gap-1">
+            <p>Release : </p>
+            <p className=" text-md">{data?.schedule.days} - {data?.schedule.time}</p>
+          </div>
+        </div>
+          
       </div>
 
-      <h2 className="text-center text-xl">{data?.name}</h2>
-      <p className="text-center text-xl">{data?.language}</p>
-      <p className="text-center text-xl">{data?.rating.average}</p>
+      <div>
+        <h2 className=" text-xl">{data?.name}</h2>
+        <p className=" text-xl">{data?.language}</p>
+        <p className=" text-xl">{data?.rating.average}</p>
 
-      <p className="text-center text-xl">{data?.summary}</p>
+        <p className=" text-xl">{data?.summary}</p>
 
-      <Button>
-        <img src="/play.svg" alt="" />
-        Play</Button>
+        <Button>
+          <img src="/play.svg" alt="" />
+          Play</Button>
 
-      <p className="text-center text-xl">{data?._links.previousepisode.name}</p>
+        <p className=" text-xl">{data?._links.previousepisode.name}</p>
+      </div>
     </div>
   );
 }
