5b2f22b833ae7ae421e5cce302edf80b7ae1194b
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-10-17 09:11:18 +0200

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,45 @@ export default function Home() {
 
   return (
     <div className=" p-5 ">
-      <div className=" relative overflow-hidden flex flex-col gap-2 pt-36 p-6 justify-center items-center rounded-lg">
-        <img className=" absolute -z-10" src={data?.image.original} alt="" />
 
-        <div className=" flex flex-col items-start justify-start w-full">
-          <p className=" text-md">{data?.genres[0]} · {data?.genres[1]} · {data?.genres[2]}</p>
-          <div className=" flex flex-row gap-1">
-            <p>Release : </p>
-            <p className=" text-md">{data?.schedule.days} - {data?.schedule.time}</p>
+      {/* Head */}
+      <div className="relative overflow-hidden flex flex-col gap-2 justify-center items-center rounded-lg h-80">
+
+        {/* Image */}
+        <img className="w-full object-cover" src={data?.image.original} alt="" />
+        {/* Fin Image */}
+
+        {/* Cadre d'opacité */}
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-dark h-20"></div>
+        {/* Fin Cadre d'opacité */}
+
+        {/* Texte */}
+        <div className="absolute bottom-4 left-6 flex flex-col items-start justify-start w-full z-10">
+          <p className="text-md text-white">{data?.genres[0]} · {data?.genres[1]} · {data?.genres[2]}</p>
+          <div className="flex flex-row gap-1">
+            <p className="text-white">Release : </p>
+            <p className="text-md text-white">{data?.schedule.days} - {data?.schedule.time}</p>
           </div>
         </div>
-          
-      </div>
+        {/* Fin Texte */}
 
+      </div>
+      {/* Fin Head */}
+
+      {/* Contenu */}
       <div>
-        <h2 className=" text-xl">{data?.name}</h2>
+        <h2 className=" text-3xl font-bold">{data?.name}</h2>
         <p className=" text-xl">{data?.language}</p>
         <p className=" text-xl">{data?.rating.average}</p>
 
         <p className=" text-xl">{data?.summary}</p>
 
-        <Button>
-          <img src="/play.svg" alt="" />
-          Play</Button>
+        <Button><img src="/play.svg" alt="" />Play</Button>
 
         <p className=" text-xl">{data?._links.previousepisode.name}</p>
       </div>
+      {/* Fin Contenu */}
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
   	extend: {
+		backgroundImage : {
+			'gradient-dark' : 'var(--gradient-dark)'
+		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
